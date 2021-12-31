01742119a1aac9c84f7d051a293c468828ffa304
ishwar.rimal@gmail.com <ishwar.rimal@gmail.com>
2021-12-31 09:57:46 +0530

    updated firebase and vite to latest version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   // your config
